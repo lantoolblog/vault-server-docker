e7f51144c465ebc727464f03be5cf7392fe53660
lantoolblog <286455583+lantoolblog@users.noreply.github.com>
2026-06-13 21:30:44 +0900

    10번체크이긴한데.. 주석을 동작에 맞게 바꾸자..
    10초 이후에도 vault 프로세스를 찾지 못했다고...

--- init-scripts/entrypoint-wrapper.sh
+++ init-scripts/entrypoint-wrapper.sh
@@ -5,7 +5,7 @@ while ! ps aux | grep -q '[v]ault server'; do
   sleep 1
   count=$((count+1))
   if [ $count -ge 10 ]; then
-    echo "Error: vault server process not found after 5 checks"
+    echo "Error: vault server process not found after 10 seconds"
     exit 1
   fi
 done
