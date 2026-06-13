15fedc4ab56a35635609825f7acee529f7f27d0c
lantoolblog <286455583+lantoolblog@users.noreply.github.com>
2026-06-14 00:48:07 +0900

    실행 구조 단순화

--- init-scripts/entrypoint-wrapper.sh
+++ init-scripts/entrypoint-wrapper.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 nohup docker-entrypoint.sh server &
+
+MAX_CHECKS=${VAULT_STARTUP_TIMEOUT:-10}
 count=0
 while ! ps aux | grep -q '[v]ault server'; do
   sleep 1
   count=$((count+1))
-  if [ $count -ge 10 ]; then
-    echo "Error: vault server process not found after 10 seconds"
+  if [ $count -ge ${MAX_CHECKS} ]; then
+    echo "Error: vault server process not found after ${MAX_CHECKS} seconds"
     exit 1
   fi
 done
 
-sh /vaultUnseal.sh
+. ./unseal-keys.properties
+vault operator unseal "${UNSEAL_KEY_1}"
+vault operator unseal "${UNSEAL_KEY_2}"
+vault operator unseal "${UNSEAL_KEY_3}"
+
 tail -f /dev/null
--- init-scripts/vaultUnseal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export VAULT_ADDR='http://127.0.0.1:8200'
-
-. /unseal-keys.properties
-
-vault operator unseal "${UNSEAL_KEY_1}"
-vault operator unseal "${UNSEAL_KEY_2}"
-vault operator unseal "${UNSEAL_KEY_3}"
